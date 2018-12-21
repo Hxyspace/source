8a3b5afe220c3f5999f3c51045eb7b54b7ac22c6
Y-mouse <1301960708@qq.com>
2018-12-21 21:57:10 +0800

    点亮LED灯

--- vsCode/C51_Code/1.LED/LED.c
+++ vsCode/C51_Code/1.LED/LED.c
@@ -1,7 +1,8 @@
-#include <reg52.h> 
-//sbit对位操作的声明，该行表示将P1.0口重新命名为LED1，对p1^0口输出为0就会发光了，操作第二个灯可设变量sbit LED2 = P1^1;
-sbit LED1 = P1^0; void main()
+#include<reg52.h>        //导入c51头文件
+
+sbit LED1 = P2^0;        //sbit 为C51单片机代码的关键字  这是C标准库没有的
+
+void main()
 {
-    //给LED1赋值0，相当于灯亮。
     LED1 = 0;
 }
