a44f544f5458db3e62a66cb7a908a353978bcf6a
Y-mouse <1301960708@qq.com>
2018-12-21 22:01:08 +0800

    开关控制LED灯

---
+++ vsCode/C51_Code/3.switchLED/switch.c
@@ -0,0 +1,14 @@
+#include <reg52.h>
+
+sbit K1 = P3 ^ 1;    //定义开关位
+
+void main()
+{
+    while (1)
+    {
+        if (K1 == 0)
+            P2 = 0x00;
+        else
+            P2 = 0xff;
+    }
+}
