dce86a5c22541438f65c23c79f581348d49af82f
Y-mouse <1301960708@qq.com>
2019-01-26 17:39:53 +0800

    希尔排序

---
+++ vsCode/C_Code/c_code/Sort/Shell_sort.c
@@ -0,0 +1,35 @@
+/*希尔排序*/
+#include<stdio.h>
+
+void shell(int k[],int N)
+{
+    int i,j,temp;
+    int gap = N;
+    do
+    {
+        gap = gap/3 + 1;
+        for(i = gap;i < N;i++)
+        {
+            if(k[i] < k[i-gap])
+            {
+                temp = k[i];
+                for(j = i-gap;k[j] > temp;j -= gap)
+                {
+                    k[j+gap] = k[j];
+                }
+                k[j+gap] = temp;
+            }
+        }
+    } while (gap > 1);
+    
+}
+
+int main()
+{
+    int k[10] = {2,3,7,9,4,1,5,0,6,8};
+    shell(k,10);
+    for(int i;i < 10;i++)
+    printf("%2d",k[i]);
+    putchar('\n');
+    return 0;;
+}
