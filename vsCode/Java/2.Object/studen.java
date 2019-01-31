e1e8616ccaef7bcb1285a2b62a3af8d83edb84b6
Y-mouse <1301960708@qq.com>
2019-01-31 18:59:52 +0800

    java

--- vsCode/Java/2.Object/studen.java
+++ vsCode/Java/2.Object/studen.java
@@ -4,6 +4,8 @@ public class studen
     String name;
     int age;
 
+    Computer comp;  //计算机
+
     void study()
     {
         System.out.println("我在学习！");
@@ -11,13 +13,26 @@ public class studen
 
     void play()
     {
-        System.out.println("我在写代码！");
+        System.out.println("我在写代码！电脑：" + comp.brand);
     }
 
     //程序执行入口
     public static void main(String[] args)
     {
         studen yuan = new studen();
+        yuan.id = 1001;
+        yuan.name = "鑫";
+        yuan.age = 20;
+
+        Computer c1 = new Computer();
+        c1.brand = "联想";
+
+        yuan.comp = c1;
         yuan.play();
     }
+}
+
+class Computer
+{
+    String brand;
 }
