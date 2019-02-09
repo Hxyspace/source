7120ad7f71deb95ad9c616434970a6ced35d89c1
Y-mouse <1301960708@qq.com>
2019-02-09 13:35:21 +0800

    程序包

---
+++ vsCode/Java/5.package/src/oo/user.java
@@ -0,0 +1,6 @@
+package oo;
+
+public class user
+{
+    public int a;
+}
---
+++ vsCode/Java/5.package/src/top/student.java
@@ -0,0 +1,13 @@
+package top;
+
+import oo.user;
+
+public class student
+{
+    public static void main(String [] args)
+    {
+        user u = new user();
+        u.a = 3;
+        System.out.println(u.a);
+    }
+}
