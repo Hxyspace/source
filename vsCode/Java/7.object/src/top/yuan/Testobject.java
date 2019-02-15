58b9fda8d5458896bc821430ba90e636a1462ed9
Y-mouse <1301960708@qq.com>
2019-02-15 14:28:44 +0800

    继承

---
+++ vsCode/Java/7.object/src/top/yuan/Testobject.java
@@ -0,0 +1,15 @@
+package top.yuan;
+
+public class Testobject
+{
+    public static void main(String[] args) 
+    {
+        Testobject to = new Testobject();
+        System.out.println(to.toString());
+    }
+
+    public String toString()
+    {
+        return "≤‚ ‘object∑Ω∑®";
+    }
+}
